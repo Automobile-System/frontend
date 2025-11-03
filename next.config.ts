289ed7aa5561d00a2006719f8c792df189d0701a
Adithya Hewage <adithyahewage69@gmail.com>
2025-11-04 02:55:29 +0530

    create files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Allow next/image to load from images.unsplash.com
+  images: {
+    domains: ["images.unsplash.com"],
+    // Alternatively you can use remotePatterns if you need fine-grained control:
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'images.unsplash.com',
+    //     port: '',
+    //     pathname: '/**',
+    //   },
+    // ],
+  },
 };
 
 export default nextConfig;
