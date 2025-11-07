9e097430e2800732ef1be5cdd8f22b498480b3b4
Thanuka Perera <155462762+ThanukaPerera@users.noreply.github.com>
2025-11-07 11:26:42 +0530

    build errors fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import LandingPage from "@/components/LandingPage"
 
+// Force dynamic rendering since we use cookies for authentication
+export const dynamic = 'force-dynamic'
+
 export default function HomePage() {
   return <LandingPage />
 }
